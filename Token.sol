9155c077653329468e1c8a76c6b875b50adbb59f
AttackOfTheClones <84107508+AttackOfTheClones@users.noreply.github.com>
2021-05-30 20:42:09 -0400

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -657,8 +657,8 @@ contract DominiDataToken is Context, IERC20, Ownable {
     uint256 private _rTotal = (MAX - (MAX % _tTotal));
     uint256 private _tFeeTotal;
 
-    string private _name = "FireToken";
-    string private _symbol = "FT";
+    string private _name = "Domini Data Token";
+    string private _symbol = "DDT";
     uint8 private _decimals = 9;
 
     uint256 public _taxFee = 75;
